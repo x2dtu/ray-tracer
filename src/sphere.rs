e6c394af7c0f2d3ddc4e3c0ec2c0e5d90af5d6c6
Michael Atkins <michael.atkins@partner.bmw.de>
2022-07-12 16:38:11 -0400

    Initial commit

--- src/sphere.rs
+++ src/sphere.rs
@@ -29,21 +29,19 @@ impl<T: Material> Hittable for Sphere<T> {
     fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
         let origin_to_center = r.origin().clone() - self.center.clone();
         let a = r.direction().length_squared();
-        let b = 2.0 * Vector3::dot(&origin_to_center, r.direction());
+        let half_b = Vector3::dot(&origin_to_center, r.direction());
         let c = origin_to_center.length_squared() - self.radius * self.radius;
 
-        let discriminant = b * b - 4.0 * a * c;
+        let discriminant = half_b*half_b - a*c;
         if discriminant < 0.0 {
             return None;
         }
+        let sqrtd = discriminant.sqrt();
 
-        // Find the nearest root that lies in the acceptable range
-        let root = (-b - discriminant.sqrt()) / (2.0 * a);
+        let mut root = (-half_b - sqrtd) / a;
         if root < t_min || root > t_max {
-            // then use other root
-            let root = (-b + discriminant.sqrt()) / (2.0 * a);
+            root = (-half_b + sqrtd) / a;
             if root < t_min || root > t_max {
-                // then both roots are not in acceptable range, so return false
                 return None;
             }
         }
